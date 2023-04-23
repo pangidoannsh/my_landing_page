6366e5188ca53150e6e934033a3749aeccac5ace
pangidoannsh <pangidoanhrp13@gmail.com>
2023-04-24 02:11:12 +0700

    del gsap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,13 @@
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { Fragment, useState } from 'react'
 import { Transition } from '@headlessui/react';
-import { gsap } from 'gsap'
+
 const defaultPosition = {
     x: 0,
     y: 0
 };
 
 export default function Hero({ isOpenMenu, scrollY, backgroundImage }) {
-    const tl = gsap.timeline()
     const [position, setPosition] = useState(defaultPosition);
-    let heroRef = useRef(null)
-    let posX = 0;
-    let posY = 0;
-    let mouseY = 0;
-    let mouseX = 0;
 
     function handleMouseMove(e) {
         // console.log('scroll');
@@ -24,31 +18,8 @@ export default function Hero({ isOpenMenu, scrollY, backgroundImage }) {
             })
         }
     }
-
-    useEffect(() => {
-        tl.to({}, 0.02, {
-            repeat: -1,
-            onRepeat: function () {
-                posX += (mouseX - posX) / 20;
-                posY += (mouseY - posY) / 20;
-                tl.set(heroRef, {
-                    css: {
-                        translateX: (window.innerWidth - (posX * 2)) / 20,
-                        translateY: (window.innerHeight - (posY * 2)) / 20,
-                    }
-                })
-            }
-        })
-        document.addEventListener('mousemove', e => {
-            if (!isOpenMenu) {
-                mouseX = e.pageX
-                mouseY = e.pageY
-            }
-        })
-    }, [isOpenMenu]);
     return (
-        // <div onMouseMove={handleMouseMove}>
-        <div>
+        <div onMouseMove={handleMouseMove}>
             <Transition as={Fragment} show={!isOpenMenu && scrollY < 300}>
                 <Transition.Child
                     as='div'
@@ -71,12 +42,12 @@ export default function Hero({ isOpenMenu, scrollY, backgroundImage }) {
 
             </Transition>
             <div className={`bg-hero-wrapper ${isOpenMenu ? 'scale-110' : ''}`}>
-                <div className={`bg-hero`} ref={ref => heroRef = ref}
+                <div className={`bg-hero`}
                     style={{
                         backgroundImage: `url('${backgroundImage}')`,
-                        // translate: `${position.x}px ${position.y}px`,
+                        translate: `${position.x}px ${position.y}px`,
                         scale: isOpenMenu ? '1.2' : '1.1',
-                        transition: 'scale 1000ms',
+                        transition: 'scale 1000ms'
                     }}
                 />
             </div>
