7b342c067506a9a9430d79de1dda38a93939f3a8
pangidoannsh <pangidoanhrp13@gmail.com>
2023-04-23 09:40:43 +0700

    fix:padding at detail

--- src/components/contents/Detail.js
+++ src/components/contents/Detail.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Detail({ description }) {
     return (
-        <div className='min-h-screen relative z-50 px-56 pt-24 text-white'>
-            <div className="px-28">
-                <h4 className='text-2xl font-semibold mb-6'>Deskripsi Projek</h4>
+        <div className='min-h-screen relative z-50 px-6 lg:px-56 pt-24 text-white'>
+            <div className="px-6 lg:px-28">
+                <h4 className='text-2xl font-semibold mb-3 lg:mb-6'>Deskripsi Projek</h4>
                 <p className='opacity-60 font-roboto'>{description}</p>
             </div>
         </div>
