57086dbc70d83eb6c28bf7057671c014675a0c86
pangidoannsh <pangidoanhrp13@gmail.com>
2023-04-23 09:24:09 +0700

    fixLupdate#6

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,14 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
 
 export default function PortfolioPage() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/')
+    }, []);
     return (
         <div />
     )
-}
-
-export async function getStaticProps(context) {
-    return {
-        redirect: {
-            destination: '/'
-        }
-    }
 }
