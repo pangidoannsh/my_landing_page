30a6ff764ef0dabe5067d323e93375fde4ad5e06
pangidoannsh <pangidoanhrp13@gmail.com>
2023-04-29 22:47:47 +0700

    fix:bug transition

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ const TL = gsap.timeline({
 });
 
 export default function Hero({ isOpenMenu, scrollY, backgroundImage, title, subTitle }) {
-    const [position, setPosition] = useState(defaultPosition);
+    // const [position, setPosition] = useState(defaultPosition);
     const heroRef = useRef(null);
 
     function handleMouseMove(e) {
@@ -32,41 +32,47 @@ export default function Hero({ isOpenMenu, scrollY, backgroundImage, title, subT
         })
     }, []);
     return (
-        // <div>
-        <div onMouseMove={handleMouseMove}>
-            <Transition as={Fragment} show={!isOpenMenu}>
-                <Transition.Child
-                    as='div'
-                    enter="ease-out duration-[400ms] delay-500"
-                    enterFrom="opacity-0 translate-y-40"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0 -translate-y-40"
-                    className={`z-10 fixed flex gap-4 flex-col px-6 justify-center items-center w-screen h-screen text-white `}
-                    style={{
-                        translate: scrollY > 50 ? `0 -${scrollY / 3.5}px` : '0 0',
-                        opacity: scrollY > 0 ? `${(200 - scrollY) / scrollY}` : '1'
-                    }}
-                >
-                    <div className="text-center text-5xl font-semibold sm:w-3/4 lg:w-1/2">
-                        {title}
-                    </div>
-                    <div className="semi-white font-roboto font-light">{subTitle}</div>
-                </Transition.Child>
+        <>
+            <div onMouseMove={handleMouseMove} className='fixed w-screen h-screen z-40' />
+            <div>
+                <Transition as={Fragment} show={!isOpenMenu}>
+                    <Transition.Child
+                        as='div'
+                        enter="ease-out duration-[400ms] delay-500"
+                        enterFrom="opacity-0 translate-y-40"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-300"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0 -translate-y-40"
+                        className={`z-10 fixed w-screen h-screen text-white `}
 
-            </Transition>
-            <div className={`bg-hero-wrapper duration-1000 ${isOpenMenu ? 'scale-105' : 'scale-100'}`}>
+                    >
+                        <div className='flex gap-4 flex-col px-6 justify-center items-center h-screen'
+                            style={{
+                                translate: scrollY > 50 ? `0 -${scrollY / 3.5}px` : '0 0',
+                                opacity: scrollY > 0 ? `${(200 - scrollY) / scrollY}` : '1'
+                            }}
+                        >
+                            <div className="text-center text-5xl font-semibold sm:w-3/4 lg:w-1/2">
+                                {title}
+                            </div>
+                            <div className="semi-white font-roboto font-light">{subTitle}</div>
+                        </div>
+                    </Transition.Child>
 
-                <div className='bg-hero' ref={heroRef}
-                    style={{
-                        backgroundImage: `url('${backgroundImage}')`,
-                        // translate: `${position.x}px ${position.y}px`,
-                        // scale: '1.1',
-                        // transition: '160ms'
-                    }}
-                />
-            </div>
-        </div >
+                </Transition>
+                <div className={`bg-hero-wrapper duration-1000 ${isOpenMenu ? 'scale-105' : 'scale-100'}`}>
+
+                    <div className='bg-hero' ref={heroRef}
+                        style={{
+                            backgroundImage: `url('${backgroundImage}')`,
+                            // translate: `${position.x}px ${position.y}px`,
+                            // scale: '1.1',
+                            // transition: '160ms'
+                        }}
+                    />
+                </div>
+            </div >
+        </>
     )
 }
